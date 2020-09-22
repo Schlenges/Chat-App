ceba51f8ab0f906f6452cef04cd357787afd85ed
Schlenges <meike_hankewicz@yahoo.de>
2020-09-23 00:16:02 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+const cors = require('cors')
+
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
 
 const PORT = process.env.PORT || 5000
@@ -10,6 +12,9 @@ const router = require('./router')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+app.use(router)
+app.use(cors())
 
 io.on('connect', (socket) => {
   socket.on('join', ({name, room}, callback) => {
@@ -39,7 +44,5 @@ io.on('connect', (socket) => {
     }
   })
 })
-
-app.use(router)
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
