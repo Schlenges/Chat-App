756d9ada5670be29121a02ad2e528bbe7e1e7c13
Schlenges <meike_hankewicz@yahoo.de>
2020-09-23 00:11:08 +0200

    add server logic

--- server/index.js
+++ server/index.js
@@ -15,13 +15,28 @@ io.on('connect', (socket) => {
   socket.on('join', ({name, room}, callback) => {
     const { user, error } = addUser({ id: socket.id, name, room})
 
-    if(error) callback(error)
+    if(error) return callback(error)
+
+    socket.emit('message', {user: 'admin', text: `${user.name} welcome to the room ${user.room}`})
+    socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined`})
 
     socket.join(user.room)
+
+    callback()
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('message', {user: user.name, text: message})
+    callback()
   })
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    const removedUser = removeUser(socket.id)
+
+    if(removedUser){
+      io.to(removedUser.room).emit('message', {user: 'admin', text: `${removedUser.name} has left`})
+    }
   })
 })
 
